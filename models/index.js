ae833c19bd846ea61f80084cb40ac3bd62288481
Justin Yocus <justin.o.yocus@gmail.com>
2020-11-12 15:48:39 -0600

    fixed model to match seed data, added index.js to help seeds file work

---
+++ models/index.js
@@ -0,0 +1,4 @@
+// this is so the seed file works
+module.exports = {
+    Workout: require('./modelWorkout')
+};
--- models/modelWorkout.js
+++ models/modelWorkout.js
@@ -2,43 +2,58 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const trackerSchema = new Schema({
-
-    name: {
-        type: String,
-        trim:true,
-        required: [true, 'Name of Workout']
-    },
-    weight: {
-        type: Number,
-        required: 'Weight (lbs)'
-    },
-    reps: {
-        type: Number,
-        required: 'How many reps?'
-    },
-    sets: {
-        type: Number,
-        required: 'How many sets?'
-    },
-    date: {
+    day: {
         type: Date,
         default: Date.now
-      }
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: [true, 'Exercise Type']
+            },
+
+            name: {
+                type: String,
+                trim: true,
+                required: [true, 'Name of Workout']
+            },
+
+            duration: {
+                type: Number,
+                required: 'Time (in minutes)'
+            },
+
+            weight: {
+                type: Number,
+
+            },
+            reps: {
+                type: Number,
+
+            },
+            sets: {
+                type: Number,
+
+            }
 
 
-});
+        }
+    ]
+})
 //Need to convert vituals to JSON
 // (
 //     toJSON:(
-            virtuals: true
+virtuals: true
 //}
 //});
 // )
 
-trackerSchema.virtual('totalDuration').get(function() {
-return this.exercises.reduce((sum, exercise) => {
-    return sum + exercise.weight
-    },0)
+trackerSchema.virtual('totalDuration').get(function () {
+    return this.exercises.reduce((sum, exercise) => {
+        return sum + exercise.weight
+    }, 0)
 });
 
 const NewWorkout = mongoose.model("Workout", trackerSchema);
